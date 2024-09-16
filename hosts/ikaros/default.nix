73bde419b9c249c8efedb7a68e7573758cfe97b6
John Smith <me@theholytachanka.com>
2024-09-16 18:39:39 +0000

    added lxc container support

--- hosts/ikaros/default.nix
+++ hosts/ikaros/default.nix
@@ -16,6 +16,7 @@
 
   # TODO: Set your hostname
   networking.hostName = "ikaros";
+  virtualisation.lxd.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
