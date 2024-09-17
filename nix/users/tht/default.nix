cbfc016c49663477801256c189f4d0bee7c25863
John Smith <me@theholytachanka.com>
2024-09-17 05:59:53 +0000

    ssh key please work

--- nix/users/tht/default.nix
+++ nix/users/tht/default.nix
@@ -11,7 +11,7 @@
   users.defaultUserShell = pkgs.fish;
   users.users = {
     root.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcTC/l7AvXzPhEJ+8PPy7NG84G7VtgJ1QWsW+xcFZgb tht"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcTC/l7AvXzPhEJ+8PPy7NG84G7VtgJ1QWsW+xcFZgb tht@viper"
     ];
     # FIXME: Replace with your username
     tht = {
@@ -21,7 +21,7 @@
       initialHashedPassword = "$6$a5OrpClAzTuokFBn$ODzSyW8pn6QEJsR1Kjsgyy.6rUqV2S865jWiDm4qXRPV26UnF29IfC6HOowInNzCRYrtFk4CwpjGAL/zni.FC/";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcTC/l7AvXzPhEJ+8PPy7NG84G7VtgJ1QWsW+xcFZgb tht"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcTC/l7AvXzPhEJ+8PPy7NG84G7VtgJ1QWsW+xcFZgb tht@viper"
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
       extraGroups = ["wheel" "audio" "video" "gamemode" "docker"];
